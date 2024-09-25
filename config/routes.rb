f031017410cba4e629019dcfd2fea092636d817d
= <=>
2024-09-25 18:12:23 +0100

    Added the Users resource (2.2)

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   
   # Defines the root path route ("/")
-  root 'application#goodbye'
+  root 'application#hello'
+  # root 'users#index'
   # root "articles#index"
 end
